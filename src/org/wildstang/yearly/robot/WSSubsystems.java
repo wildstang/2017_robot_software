6c9c5d197a6f98134a24e25f090eb9627de493a3
sgarward <sgarward@us.ibm.com>
2017-01-09 10:38:43 -0600

    Initial commit for 2017. 2016 code import minus most robot specific code. Updated for new WPILib and CTRE library.

---
+++ src/org/wildstang/yearly/robot/WSSubsystems.java
@@ -0,0 +1,41 @@
+package org.wildstang.yearly.robot;
+
+import org.wildstang.framework.core.Subsystems;
+import org.wildstang.yearly.subsystems.DriveBase;
+
+public enum WSSubsystems implements Subsystems
+{
+
+   //DO NOT REMOVE THIS COMMENT.  DO NOT PLACE ANY ENUMERATION DEFINITIONS IN FRONT OF IT.
+   //This keeps the formatter from completely making the enumeration unreadable.
+   // @formatter::off
+//   MONITOR("Monitor", Monitor.class),
+   DRIVE_BASE("Drive Base", DriveBase.class);
+//   ENCODER_TEST("Encoder Test", EncoderTest.class);
+   
+   //DO NOT REMOVE THIS COMMENT.  DO NOT PLACE ANY ENUMERATION DEFINITIONS AFTER IT.
+   //This keeps the formatter from completely making the enumeration unreadable.
+   // @formatter::on
+   
+   private String m_name;
+   private Class m_class;
+
+   WSSubsystems(String p_name, Class p_class)
+   {
+      m_name = p_name;
+      m_class = p_class;
+   }
+
+   @Override
+   public String getName()
+   {
+      return m_name;
+   }
+
+   @Override
+   public Class getSubsystemClass()
+   {
+      return m_class;
+   }
+
+}
