ac62d77125e992d887d5b53008ab578910b038e6
Alex Guerra <aguerra7002@stu.d214.org>
2017-01-12 17:33:46 -0800

    Added DriveStateClass so we can monitor driving over time.

---
+++ src/org/wildstang/yearly/robot/DriveState.java
@@ -0,0 +1,54 @@
+package org.wildstang.yearly.robot;
+
+public class DriveState {
+	
+	private double deltaLeftEncoderTicks;
+	private double deltaRightEncoderTicks;
+	private double deltaTimeMS;
+	private double headingAngle;
+	
+	public DriveState() {
+		
+	}
+	
+	public DriveState(double deltaTime, double deltaRightTicks, 
+			double deltaLeftTicks, double heading) {
+		deltaTimeMS = deltaTime;
+		deltaRightEncoderTicks = deltaRightTicks;
+		deltaLeftEncoderTicks = deltaLeftTicks;
+		headingAngle = heading;
+	}
+	
+	public void setDeltaTime(double time) {
+		deltaTimeMS = time;
+	}
+	
+	public double getDeltaTime() {
+		return deltaTimeMS;
+	}
+	
+	public void setDeltaRightEncoderTicks(double ticks) {
+		deltaRightEncoderTicks = ticks;
+	}
+	
+	public double getDeltaRightEncoderTicks() {
+		return deltaRightEncoderTicks;
+	}
+	
+	public void setDeltaLeftEncoderTicks(double ticks) {
+		deltaLeftEncoderTicks = ticks;
+	}
+	
+	public double getDeltaLeftEncoderTicks() {
+		return deltaLeftEncoderTicks;
+	}
+	
+	public void setHeading(double heading) {
+		headingAngle = heading;
+	}
+	
+	public double getHeadingAngle() {
+		return headingAngle;
+	}
+
+}
