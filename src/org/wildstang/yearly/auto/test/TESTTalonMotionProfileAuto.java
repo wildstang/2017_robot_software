9eabaaa7890019227111a824457c999fd39d477f
Alex Guerra <aguerra7002@stu.d214.org>
2017-01-23 17:56:06 -0800

    cleaned up config file. added probably bad code that reads path from config file. Definitely is not 100% right, but mostly wrote it as a starting point.

--- src/org/wildstang/yearly/auto/test/TESTTalonMotionProfileAuto.java
+++ src/org/wildstang/yearly/auto/test/TESTTalonMotionProfileAuto.java
@@ -1,14 +1,32 @@
 package org.wildstang.yearly.auto.test;
 
+import java.io.File;
+
 import org.wildstang.framework.auto.AutoProgram;
+import org.wildstang.framework.core.Core;
 
 public class TESTTalonMotionProfileAuto extends AutoProgram
 {
 
+	boolean isRed;
+	
+	
    @Override
    protected void defineSteps()
-   {
-      addStep(new PathFollowerStep("/home/lvuser/path.test.txt"));
+   { 
+	  isRed = false; // Need to change this to a Driver Station input 
+	  String auto;
+	  if (isRed) {
+		  auto = Core.getConfigManager().getConfig().getString("boiler.to.hopper.auto.red", "defaultRed");
+	  } else {
+	      auto = Core.getConfigManager().getConfig().getString("boiler.to.hopper.auto.blue", "defaultBlue");
+	  }
+	  
+	  String autoRight = auto + ".right";
+	  String autoLeft = auto + ".left";
+	  
+	  addStep(new PathFollowerStep(autoRight));
+      addStep(new PathFollowerStep(autoLeft));
    }
 
    @Override
