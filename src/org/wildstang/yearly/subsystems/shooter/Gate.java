26cdc4906938c5675111c3da96a7662563b84dee
6henr <6henry@gmail.com>
2017-01-28 10:02:26 -0600

    Added WsSolenoid to imports

--- src/org/wildstang/yearly/subsystems/shooter/Gate.java
+++ src/org/wildstang/yearly/subsystems/shooter/Gate.java
@@ -2,6 +2,7 @@ package org.wildstang.yearly.subsystems.shooter;
 
 import org.wildstang.yearly.subsystems.Shooter;
 import edu.wpi.first.wpilibj.Solenoid;
+import org.wildstang.hardware.crio.outputs.WsSolenoid;
 
 public class Gate extends Shooter
 {
