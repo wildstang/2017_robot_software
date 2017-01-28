01d06f5d00163f6d61d0471e83826ad4fd836556
Becky <Becky@Becca>
2017-01-28 08:53:24 -0600

    Changed getState to isRunning

--- src/org/wildstang/yearly/subsystems/shooter/Flywheel.java
+++ src/org/wildstang/yearly/subsystems/shooter/Flywheel.java
@@ -4,43 +4,42 @@ import org.wildstang.yearly.subsystems.Shooter;
 
 import com.ctre.CANTalon;
 
-public class Flywheel extends Shooter {
-	
-	private CANTalon m_talon;
-	private double m_speed;
-	
-	public Flywheel (CANTalon p_talon){
-		m_talon = p_talon;
-		
-	}
-	
-	public void turnOn() {
-		m_talon.set(m_speed);
-	}
+public class Flywheel extends Shooter
+{
 
-	
-    public void turnOff() {
-		
-	}
-    
-    
-    public boolean getState() {
-    	
-    	boolean state = false;
-    	
-    	return state;
-    }
-    
-    
-    public double getSpeed() {
-    	return m_talon.getSpeed();
-    }
-    
-    
-    public void setSpeed( double p_wheel  ) {
-    	
-    	
-    }
+   private CANTalon m_talon;
+   private double m_speed;
 
+   public Flywheel(CANTalon p_talon)
+   {
+      m_talon = p_talon;
+
+   }
+
+   public void turnOn()
+   {
+      m_talon.set(m_speed);
+   }
+
+   public void turnOff()
+   {
+      m_talon.set(0);
+   }
+
+   public boolean isRunning()
+   {
+      m_talon.
+      return false;
+   }
+
+   public double getSpeed()
+   {
+      return m_talon.getSpeed();
+   }
+
+   public void setSpeed(double p_wheel)
+   {
+
+   }
 
 }
