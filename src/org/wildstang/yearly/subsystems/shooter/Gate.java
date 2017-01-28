e7b2106041384bc13893de9dacb83aab907e61f9
Becky <Becky@Becca>
2017-01-28 09:52:33 -0600

    Completed most of the Gate preset functions

--- src/org/wildstang/yearly/subsystems/shooter/Gate.java
+++ src/org/wildstang/yearly/subsystems/shooter/Gate.java
@@ -3,29 +3,36 @@ package org.wildstang.yearly.subsystems.shooter;
 import org.wildstang.yearly.subsystems.Shooter;
 import edu.wpi.first.wpilibj.Solenoid;
 
-public class Gate extends Shooter {
-   
-   private Solenoid m_solinoid;
-	
-	void openGate() {
-		
-	}
-	
-	void closeGate() {
-		
-		
-	}
+public class Gate extends Shooter
+{
 
-	boolean isOpen() {
-		boolean state = false;
-		
-		return state;
-	}
-	
-	boolean isClosed() {
-		boolean state = false;
-		
-		return state;
-		
-	}
+   private Solenoid m_solenoid;
+
+   public Gate(Solenoid p_solenoid)
+   {
+      m_solenoid = p_solenoid;
+   }
+
+   void openGate()
+   {
+      m_solenoid.set(true);
+   }
+
+   void closeGate()
+   {
+      m_solenoid.set(false);
+   }
+
+   boolean isOpen()
+   {
+      boolean state = false;
+
+      return state;
+   }
+
+   boolean isClosed()
+   {
+      return false;
+
+   }
 }
