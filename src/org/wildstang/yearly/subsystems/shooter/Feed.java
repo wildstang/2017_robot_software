0ceaafc8882e1c63973ebb3fd8f74b32d1f5b999
Becky <Becky@Becca>
2017-01-28 07:50:46 -0600

    Template of Shooter Objects

---
+++ src/org/wildstang/yearly/subsystems/shooter/Feed.java
@@ -0,0 +1,28 @@
+package org.wildstang.yearly.subsystems.shooter;
+
+import org.wildstang.yearly.subsystems.Shooter;
+
+public class Feed extends Shooter {
+	
+	boolean isJammed() {
+		boolean state = false;
+		
+		return state;
+		
+	}
+	
+	void runForward() {
+		
+		
+	}
+
+	
+	void runBackwards() {
+		
+	}
+	
+	void stop() {
+		
+		
+	}
+}
