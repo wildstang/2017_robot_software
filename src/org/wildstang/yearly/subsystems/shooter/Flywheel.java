e35817083c3e576f9408badbb6b2c526c2867b49
Toshiba <Toshiba@192.168.1.205>
2017-01-28 09:19:45 -0600

    Updated WSInputs and WSOutputs

--- src/org/wildstang/yearly/subsystems/shooter/Flywheel.java
+++ src/org/wildstang/yearly/subsystems/shooter/Flywheel.java
@@ -4,43 +4,40 @@ import org.wildstang.yearly.subsystems.Shooter;
 
 import com.ctre.CANTalon;
 
-public class Flywheel extends Shooter {
-	
-	private CANTalon m_talon;
-	private double m_speed;
-	
-	public Flywheel (CANTalon p_talon){
-		m_talon = p_talon;
-		
-	}
-	
-	public void turnOn() {
-		m_talon.set(m_speed);
-	}
+public class Flywheel extends Shooter
+{
 
-	
-    public void turnOff() {
-		
-	}
-    
-    
-    public boolean getState() {
-    	
-    	boolean state = false;
-    	
-    	return state;
-    }
-    
-    
-    public double getSpeed() {
-    	return m_talon.getSpeed();
-    }
-    
-    
-    public void setSpeed( double p_wheel  ) {
-    	
-    	
-    }
+   private CANTalon m_talon;
+   private double m_speed;
 
+   public Flywheel(CANTalon p_talon)
+   {
+      m_talon = p_talon;
+   }
+
+   public void turnOn()
+   {
+      m_talon.set(m_speed);
+   }
+
+   public void turnOff()
+   {
+      m_talon.set(0);
+   }
+
+   public boolean isRunning()
+   {
+      return m_talon.isEnabled();
+   }
+
+   public double getSpeed()
+   {
+      return m_talon.getSpeed();
+   }
+
+   public void setSpeed(double p_wheelSpeed)
+   {
+      m_talon.set(p_wheelSpeed);
+   }
 
 }
