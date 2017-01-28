feaa8d6ebf8d322d080bc0bec6a34ad39b4c2fd0
Becky <Becky@Becca>
2017-01-28 09:36:21 -0600

    Created a solenoid variable.

--- src/org/wildstang/yearly/subsystems/shooter/Gate.java
+++ src/org/wildstang/yearly/subsystems/shooter/Gate.java
@@ -1,8 +1,11 @@
 package org.wildstang.yearly.subsystems.shooter;
 
 import org.wildstang.yearly.subsystems.Shooter;
+import edu.wpi.first.wpilibj.Solenoid;
 
 public class Gate extends Shooter {
+   
+   private Solenoid m_solinoid;
 	
 	void openGate() {
 		
