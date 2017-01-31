a10af2af58bd1fd02c4394d9003554c354a8dffb
6henr <6henry@gmail.com>
2017-01-30 20:13:14 -0600

    added setPID method to flywheel talon

--- src/org/wildstang/yearly/subsystems/shooter/Flywheel.java
+++ src/org/wildstang/yearly/subsystems/shooter/Flywheel.java
@@ -3,16 +3,25 @@ package org.wildstang.yearly.subsystems.shooter;
 import org.wildstang.yearly.subsystems.Shooter;
 
 import com.ctre.CANTalon;
+import edu.wpi.first.wpilibj.CounterBase.EncodingType;
+import edu.wpi.first.wpilibj.Encoder;
 
 public class Flywheel extends Shooter
 {
 
    private CANTalon m_talon;
+
    private double m_speed;
+
+   private double p; // Proportional
+   private double i; // Integral
+   private double d; // Derivative
 
    public Flywheel(CANTalon p_talon)
    {
       m_talon = p_talon;
+      m_talon.setPID(p, i, d); // Set the PID constants (p, i, d)
+
    }
 
    public void turnOn()
@@ -38,6 +47,13 @@ public class Flywheel extends Shooter
    public void setSpeed(double p_wheelSpeed)
    {
       m_talon.set(p_wheelSpeed);
+   }
+
+   public void setTalonPID()
+   {
+
+      m_talon.setPID(p, i, d); // Set the PID constants (p, i, d)
+
    }
 
 }
