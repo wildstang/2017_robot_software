abf0fc93e146362233870ec00126b2ad1597803d
Becky <Becky@Becca>
2017-02-03 12:35:18 -0600

    Added in comments and BENOs, as well as simplified the code

--- src/org/wildstang/yearly/subsystems/shooter/Gate.java
+++ src/org/wildstang/yearly/subsystems/shooter/Gate.java
@@ -5,23 +5,35 @@ import org.wildstang.hardware.crio.outputs.WsSolenoid;
 
 public class Gate extends Shooter
 {
-
+   // Needs comments
    private WsSolenoid m_solenoid;
+
+   private boolean OPEN_STATE = true;
+
+   // Creating a gate object so that both gates can be declared in the Shooter
+   // subclass
+   // as well as mutated accordingly with the functions below
 
    public Gate(WsSolenoid p_solenoid)
    {
       m_solenoid = p_solenoid;
    }
 
+   // Opens the gate by setting the solenoid state to true
+
    public void openGate()
    {
-      m_solenoid.setValue(true);
+      m_solenoid.setValue(OPEN_STATE);
    }
+
+   // Closes the gate by setting the solenoid state to false
 
    public void closeGate()
    {
-      m_solenoid.setValue(false);
+      m_solenoid.setValue(!OPEN_STATE);
    }
+
+   // Returns the current state/position of the gate
 
    public boolean isOpen()
    {
@@ -30,8 +42,12 @@ public class Gate extends Shooter
 
    }
 
-   public boolean isClosed()
-   {
-      return !(m_solenoid.getValue());
-   }
+   // BeNo: This funcion is not really needed, should consider removing!!
+
+   // public boolean isClosed()
+   // {
+   //
+   // return !isOpen();
+   //
+   // }
 }
