1ce542bd362a26fdfbd0979eba308de23d590823
6henr <6henry@gmail.com>
2017-02-04 11:04:34 -0600

    added a shoot step for auto

--- src/org/wildstang/yearly/auto/steps/ShootStep.java
+++ src/org/wildstang/yearly/auto/steps/ShootStep.java
@@ -5,32 +5,28 @@ import org.wildstang.framework.core.Core;
 import org.wildstang.yearly.robot.WSSubsystems;
 import org.wildstang.yearly.subsystems.Shooter;
 
-public class OpenGateStep extends AutoStep
+public class ShootStep extends AutoStep
 {
+
    private Shooter shooter;
-   
+
    @Override
    public void initialize()
    {
-      shooter = (Shooter)Core.getSubsystemManager().getSubsystem(WSSubsystems.SHOOTER.getName());
+      shooter = (Shooter) Core.getSubsystemManager().getSubsystem(WSSubsystems.SHOOTER.getName());
    }
 
    @Override
    public void update()
    {
-      
-     shooter.openBothGate();
-     
-     
-     setFinished(true);
-     isFinished();
-
+       
    }
 
    @Override
    public String toString()
    {
-      return "Open Gate Step";
+
+      return "Shoot Step";
    }
 
 }
