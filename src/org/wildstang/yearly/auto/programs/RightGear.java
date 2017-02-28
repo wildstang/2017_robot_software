70735712cf05214d6e45201948082b9b97299f2a
Becky <Becky@Becca>
2017-02-27 20:11:26 -0600

    Making some auto stuff

---
+++ src/org/wildstang/yearly/auto/programs/RightGear.java
@@ -0,0 +1,22 @@
+package org.wildstang.yearly.auto.programs;
+
+import org.wildstang.framework.auto.AutoProgram;
+
+public class RightGear extends AutoProgram
+{
+
+   @Override
+   protected void defineSteps()
+   {
+      // TODO Auto-generated method stub
+      
+   }
+
+   @Override
+   public String toString()
+   {
+      // TODO Auto-generated method stub
+      return "RightGear";
+   }
+
+}
