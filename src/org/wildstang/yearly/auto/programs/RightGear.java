7de74f1f2a11e6cd5f9a51eca551de927e5c4026
Becky <Becky@Becca>
2017-03-01 18:09:22 -0600

    Filled in some path stuff

--- src/org/wildstang/yearly/auto/programs/RightGear.java
+++ src/org/wildstang/yearly/auto/programs/RightGear.java
@@ -1,6 +1,13 @@
 package org.wildstang.yearly.auto.programs;
 
 import org.wildstang.framework.auto.AutoProgram;
+import org.wildstang.framework.config.Config;
+import org.wildstang.framework.core.Core;
+import org.wildstang.yearly.auto.steps.CloseGearHolderStep;
+import org.wildstang.yearly.auto.steps.DeliverGearStep;
+import org.wildstang.yearly.auto.steps.OpenGearHolderStep;
+import org.wildstang.yearly.auto.steps.PathFollowerStep;
+import org.wildstang.yearly.auto.steps.TrackVisionToGearStep;
 
 public class RightGear extends AutoProgram
 {
@@ -8,7 +15,16 @@ public class RightGear extends AutoProgram
    @Override
    protected void defineSteps()
    {
-      // TODO Auto-generated method stub
+      Config config = Core.getConfigManager().getConfig();
+
+      addStep(new PathFollowerStep(config.getString("RIGHT_GEAR_PATH", "")));
+      addStep(new TrackVisionToGearStep());
+      addStep(new DeliverGearStep());
+      addStep(new OpenGearHolderStep());
+
+      // TODO: Drive away
+
+      addStep(new CloseGearHolderStep());
       
    }
 
@@ -16,7 +32,7 @@ public class RightGear extends AutoProgram
    public String toString()
    {
       // TODO Auto-generated method stub
-      return "RightGear";
+      return "Right Gear";
    }
 
 }
--- src/org/wildstang/yearly/auto/programs/ShootGear.java
+++ src/org/wildstang/yearly/auto/programs/ShootGear.java
@@ -1,14 +1,45 @@
 package org.wildstang.yearly.auto.programs;
 
 import org.wildstang.framework.auto.AutoProgram;
+import org.wildstang.framework.core.Core;
+import org.wildstang.yearly.auto.steps.DeliverGearStep;
+import org.wildstang.yearly.auto.steps.FeedOnStep;
+import org.wildstang.yearly.auto.steps.OpenGearHolderStep;
+import org.wildstang.yearly.auto.steps.PathFollowerStep;
+import org.wildstang.yearly.auto.steps.ShootStep;
+import org.wildstang.yearly.auto.steps.ShooterOff;
+import org.wildstang.yearly.auto.steps.ShooterOnAndReady;
+import org.wildstang.yearly.auto.steps.TrackVisionToGearStep;
+import org.wildstang.yearly.auto.steps.WaitStep;
 
 public class ShootGear extends AutoProgram
 {
 
+   private double delayWhileShooting;
+
    @Override
+   public void initialize()
+   {
+      super.initialize();
+      
+      // Read config values
+      delayWhileShooting = Core.getConfigManager().getConfig().getDouble(this.getClass().getName() + ".delayWhileShooting", 2000);
+   }
    protected void defineSteps()
    {
       // TODO Auto-generated method stub
+      addStep(new ShooterOnAndReady());
+      addStep(new ShootStep());
+      
+      addStep(new WaitStep(delayWhileShooting));
+      
+      addStep(new ShooterOff());
+      
+//       TODO Correct Paths and Tracker, if need be
+//      addStep(new PathFollowerStep(config.getString("AUTO_PATH", "")));
+//      addStep(new TrackVisionToGearStep());
+//      addStep(new DeliverGearStep());
+//      addStep(new OpenGearHolderStep());
       
    }
 
@@ -16,7 +47,7 @@ public class ShootGear extends AutoProgram
    public String toString()
    {
       // TODO Auto-generated method stub
-      return null;
+      return "Shoot Gear";
    }
 
 }
