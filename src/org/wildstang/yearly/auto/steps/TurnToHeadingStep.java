79fa71b9d397d638505990e6af8de1d5e0214332
Steve Garward <garwards@gmail.com>
2017-03-20 23:18:33 -0500

    Added PID controller to use in auto step. Untested.

--- src/org/wildstang/yearly/auto/steps/TurnToHeadingStep.java
+++ src/org/wildstang/yearly/auto/steps/TurnToHeadingStep.java
@@ -14,19 +14,41 @@ public class TurnToHeadingStep extends AutoStep
    private PIDController m_PidController = null;
    private WsCompassInput m_compass;
    private Drive m_drive;
+   private boolean m_started = false;
+   private double m_heading;
    
+   public TurnToHeadingStep(double p_heading)
+   {
+      m_heading = p_heading;
+   }
    
    @Override
    public void initialize()
    {
       m_compass = (WsCompassInput)Core.getInputManager().getInput(WSInputs.IMU.getName());
       m_drive = (Drive)Core.getSubsystemManager().getSubsystem(WSSubsystems.DRIVE_BASE.getName());
-      m_PidController = new PIDController(0.01, 0, 0, m_compass, m_drive);
+      m_PidController = new PIDController(0.03, 0, 0, m_compass, m_drive);
+      
+      m_PidController.setAbsoluteTolerance(2.0);
+      m_PidController.setInputRange(0, 360);
+      m_PidController.setContinuous();
+      m_PidController.setSetpoint(m_heading);
+      m_PidController.setOutputRange(-1.0, 1.0);
    }
 
    @Override
    public void update()
    {
+      if (!m_started)
+      {
+         m_PidController.enable();
+         m_started = true;
+      }
+      
+      if (m_PidController.onTarget())
+      {
+         setFinished(true);
+      }
    }
 
    @Override
