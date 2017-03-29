f4652b2ceba4ce023d63ee0f658dc1115dc5f7f4
sgarward <sgarward@9.47.28.226>
2017-03-29 15:54:58 -0500

    Added code for additional USB camera. Commented out for now.

--- src/org/wildstang/yearly/robot/RobotTemplate.java
+++ src/org/wildstang/yearly/robot/RobotTemplate.java
@@ -23,6 +23,7 @@ import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+//import org.opencv.core.Mat;
 import org.wildstang.framework.auto.AutoManager;
 import org.wildstang.framework.core.Core;
 import org.wildstang.framework.logger.StateLogger;
@@ -41,6 +42,10 @@ import org.wildstang.yearly.robot.vision.VisionServer;
 import org.wildstang.yearly.subsystems.Drive;
 import org.wildstang.yearly.subsystems.LED;
 
+//import edu.wpi.cscore.CvSink;
+//import edu.wpi.cscore.CvSource;
+//import edu.wpi.cscore.UsbCamera;
+//import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
@@ -210,6 +215,27 @@ public class RobotTemplate extends IterativeRobot
 
       s_log.logp(Level.ALL, this.getClass().getName(), "robotInit", "Startup Completed");
 
+/******************
+      // Untested!!  Set up USB camera as additional vision back to dashboard.
+      // Processing should not be necessary, but code is here for reference.
+      // Code used is to resize for bandwidth restrictions in case setResolution() is not sufficient.
+//      new Thread(() -> {
+         UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
+         camera.setResolution(320, 240);
+         
+//         CvSink cvSink = CameraServer.getInstance().getVideo();
+//         CvSource outputStream = CameraServer.getInstance().putVideo("Blur", 640, 480);
+//         
+//         Mat source = new Mat();
+//         Mat output = new Mat();
+//         
+//         while(!Thread.interrupted()) {
+//             cvSink.grabFrame(source);
+//             outputStream.putFrame(output);
+//         }
+//     }).start();
+*******************/
+      
       startupTimer.endTimingSection();
 
    }
