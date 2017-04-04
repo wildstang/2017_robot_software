d357c333988c2ad7252ede3021bae45df5589fa0
Becky <Becky@192.168.1.31>
2017-04-03 20:23:58 -0500

    Starting up the second version for gear mech

---
+++ src/org/wildstang/yearly/subsystems/GearV2.java
@@ -0,0 +1,17 @@
+package org.wildstang.yearly.subsystems;
+
+import org.wildstang.framework.core.Core;
+import org.wildstang.framework.io.Input;
+import org.wildstang.framework.io.inputs.DigitalInput;
+import org.wildstang.framework.subsystems.Subsystem;
+import org.wildstang.hardware.crio.outputs.WsSolenoid;
+import org.wildstang.yearly.robot.WSInputs;
+import org.wildstang.yearly.robot.WSOutputs;
+
+public class GearV2
+{
+   private boolean prprprprprprprpr;
+   private boolean eir;
+   private boolean eer;
+   private boolean cheeeeeeeer;
+}
