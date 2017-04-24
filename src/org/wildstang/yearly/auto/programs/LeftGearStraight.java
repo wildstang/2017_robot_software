22f48f11eb5fb0fe48a8cc439f8514376016cb7a
Steve Garward <garwards@gmail.com>
2017-04-23 22:28:32 -0500

    Created new left and right straight driving versions.

---
+++ src/org/wildstang/yearly/auto/programs/LeftGearStraight.java
@@ -0,0 +1,26 @@
+package org.wildstang.yearly.auto.programs;
+
+import org.wildstang.framework.auto.AutoProgram;
+import org.wildstang.yearly.auto.steps.MotionMagicStraightLine;
+import org.wildstang.yearly.auto.steps.SideGearStepGroup;
+
+public class LeftGearStraight extends AutoProgram
+{
+
+   @Override
+   protected void defineSteps()
+   {
+      addStep(new SideGearStepGroup(60, 72));
+
+      // Go backwards 2ft
+      addStep(new MotionMagicStraightLine(-24));
+
+   }
+
+   @Override
+   public String toString()
+   {
+      return "Left Gear";
+   }
+
+}
